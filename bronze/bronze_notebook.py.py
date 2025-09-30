1836fc201a271616f32a55626787e35738520e03
MateusRamosv2 <MateusRamosv2>
2025-09-30 21:21:31 +0000

    Conversão dos dados crus para Parquet

--- bronze/bronze_notebook.py.py
+++ bronze/bronze_notebook.py.py
@@ -6,3 +6,7 @@ caminho_DataSet = "/Volumes/workspace/default/arquivos-projetos/coin_Bitcoin.csv
 df_bronze = pd.read_csv(caminho_DataSet)
 
 display(df_bronze.head)
+
+
+caminho_parquet = "../bronze/coin_bitcoin_bronze.parquet"
+df_bronze.to_parquet(caminho_parquet, index=False)
