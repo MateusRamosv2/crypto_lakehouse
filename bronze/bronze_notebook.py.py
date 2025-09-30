00252eea2dddd99dafa75112972439bdf6a522a1
MateusRamosv2 <MateusRamosv2>
2025-09-30 21:18:03 +0000

    Importação dos dados crus

--- bronze/bronze_notebook.py.py
+++ bronze/bronze_notebook.py.py
@@ -1,2 +1,8 @@
 # Databricks notebook source
+import pandas as pd
 
+caminho_DataSet = "/Volumes/workspace/default/arquivos-projetos/coin_Bitcoin.csv"
+
+df_bronze = pd.read_csv(caminho_DataSet)
+
+display(df_bronze.head)
