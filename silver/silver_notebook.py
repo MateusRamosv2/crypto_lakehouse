4b05de0c858282a14e9e5a4e4c38127add84f5b2
MateusRamosv2 <MateusRamosv2>
2025-10-02 20:50:11 +0000

    Limpeza e padronização de dados

--- silver/silver_notebook.py
+++ silver/silver_notebook.py
@@ -1,2 +1,33 @@
 # Databricks notebook source
+import pandas as pd
 
+caminho_bronze = "../bronze/coin_bitcoin_bronze.parquet"
+
+df_silver = pd.read_parquet(caminho_bronze)
+
+df_silver.columns = (
+    df_silver.columns.str.strip()
+    .str.lower()                   
+    .str.replace(" ", "_")         
+    .str.replace("-", "_")         
+)
+
+for col in df_silver:
+    if "date" in col or "time" in col:
+        df_silver[col] = pd.to_datetime(df_silver[col], errors= "coerce")
+
+df_silver = df_silver.dropna(how="all")
+df_silver = df_silver.fillna(method="ffill")
+
+df_silver = df_silver.rename(columns={"date": "data", "sno": "ano", "name": "nome", "symbol": "simbolo", "high": "preco_alto",
+                                      "low": "preco_baixo", "open": "preco_abertura", "close": "preco_fechamento", "marketcap": "capitalizacao"})
+
+
+df_silver['data'] = df_silver['data'].dt.strftime('%Y-%m-%d')
+df_silver = df_silver.drop_duplicates()
+
+
+# display(df_silver)
+
+# caminho_parquet = "../silver/coin_bitcoin_silver.parquet"
+# df_silver.to_parquet(caminho_parquet, index=False)
