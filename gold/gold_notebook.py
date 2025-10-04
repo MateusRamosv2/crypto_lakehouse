c71a56913d947d5ad948a35620f990d953e2e407
MateusRamosv2 <MateusRamosv2>
2025-10-04 15:56:17 +0000

    Enriquecimento do DataSet com métricas financeiras

--- gold/gold_notebook.py
+++ gold/gold_notebook.py
@@ -7,4 +7,11 @@ spark = SparkSession.builder.getOrCreate()
 
 caminho_silver = "../silver/coin_bitcoin_silver.parquet"
 df_gold = pd.read_parquet(caminho_silver)
+
+df_gold["retorno_pct"] = df_gold["preco_fechamento"].pct_change() * 100
+
+df_gold["volatilidade"] = df_gold["preco_alto"] - df_gold["preco_baixo"]
+
+df_gold["media_movel_7d"] = df_gold["preco_fechamento"].rolling(window=7).mean()
+
 display(df_gold)
