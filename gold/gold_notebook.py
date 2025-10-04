4030056ad29b6463d1b2fa2f31a97601e86e55bd
MateusRamosv2 <MateusRamosv2>
2025-10-04 15:45:34 +0000

    Ingestão dos dados tratados notebook gold

--- gold/gold_notebook.py
+++ gold/gold_notebook.py
@@ -1,2 +1,10 @@
 # Databricks notebook source
+import pandas as pd
+from pyspark.sql import SparkSession
 
+# Inicializa Spark (necessário para usar SQL no Databricks)
+spark = SparkSession.builder.getOrCreate()
+
+caminho_silver = "../silver/coin_bitcoin_silver.parquet"
+df_gold = pd.read_parquet(caminho_silver)
+display(df_gold)
